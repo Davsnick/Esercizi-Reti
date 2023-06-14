6aba30b3ba87415bea0f835b33f9cc5926dd75fb
Davsnick <70839783+Davsnick@users.noreply.github.com>
2023-06-14 12:50:25 +0200

    Add files via upload

--- Templates/client.c
+++ Templates/client.c
@@ -5,62 +5,69 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <netdb.h> 
 
 void error(char *msg){
-	perror(msg);
-	exit(0);
+    perror(msg);
+    exit(0);
 }
 
-/*		CLIENT		*/
+/*
+	The BSD server creates a socket, uses bind to attach that socket to a port,
+	and configures it as a listening socket.
+	This allows the server to receive incoming connection requests.
+	Afterwards, accept is called, which will block the socket,
+	until an incoming connection request is received
+*/
 int main(int argc, char *argv[]){
-
-	int sockfd, portno, n;
+    int sockfd, portno, n;
 	char buffer[256];
+	/* sockaddr_in: IPv4 Socket Address structure */
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
+	// control arguments
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
 
-	if (argc < 3) {
-		fprintf(stderr,"usage %s hostname port\n", argv[0]);
-		exit(0);
-	}
+	/*
+		SOCKET
+		Creates a communication socket
+		AF_INET means using IPv4
+		SOCK_STREAM means using TCP
+		use SOCK_DGRAM for UDP
+		INADDR_ANY means all IP addresses accepted
 
-	//port number
-	portno = atoi(argv[2]);
+		int socket(family, type, protocol)
+	*/
+    if (sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+		error("ERROR opening socket");
 
-	//descriptor, type: SOCK_STREAM o SOCK_DGRAM
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {error("ERROR opening socket");}
+    if (server = gethostbyname(argv[1]) == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(const struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
 
-	server = gethostbyname(argv[1]);
-	if (server == NULL) {
-        	fprintf(stderr, "ERROR, no such host\n");
-        	exit(0);
-	}
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
+    if (write(sockfd, buffer, strlen(buffer)) < 0)
+         error("ERROR writing to socket");
 
-	//connect(descriptor, server address, address lenght), operazione per contattare un server che ha chiamato accept(), inizia la connessione
-	if (connect(sockfd, (const struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {error("ERROR connecting");}
 
-	printf("Please enter the message: ");
-	bzero(buffer, 256);
-	fgets(buffer, 255, stdin);
-
-	//scrittura sulla socket
-	n = write(sockfd,buffer,strlen(buffer));
-	if (n < 0) {error("ERROR writing to socket");}
-
-	bzero(buffer,256);
-
-	//lettura dalla socket
-	n = read(sockfd,buffer,255);
-	printf("%s\n",buffer);
-	if (n < 0) {error("ERROR reading from socket");}
-
-	return 0;
+    bzero(buffer,256);
+    if (n = read(sockfd, buffer, 255) < 0)
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    return 0;
 }
